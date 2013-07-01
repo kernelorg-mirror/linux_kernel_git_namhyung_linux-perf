34ffb5557f22aa3c9cde73c9d5f422ce925bd155
Namhyung Kim <namhyung.kim@lge.com>
2013-07-01 13:44:32 +0900

    tracing/kprobes: Make stack and memory fetch functions static
    Those fetch functions need to be implemented differently for kprobes
    and uprobes.  Since the symbol and deref fetch functions don't call
    those directly anymore, we can make them static and implement them
    separately.
    
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: zhangwei(Jovi) <jovi.zhangwei@huawei.com>
    Cc: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- kernel/trace/trace_probe.h
+++ kernel/trace/trace_probe.h
@@ -171,23 +171,15 @@ DECLARE_FETCH_FUNC(method, u32);		\
 DECLARE_FETCH_FUNC(method, u64)
 
 /*
- * Declare fetch functions
+ * Declare common fetch functions for both of kprobes and uprobes
  */
 DECLARE_BASIC_FETCH_FUNCS(reg);
 #define fetch_reg_string		NULL
 #define fetch_reg_string_size		NULL
 
-DECLARE_BASIC_FETCH_FUNCS(stack);
-#define fetch_stack_string		NULL
-#define fetch_stack_string_size		NULL
-
 DECLARE_BASIC_FETCH_FUNCS(retval);
 #define fetch_retval_string		NULL
 #define fetch_retval_string_size	NULL
-
-DECLARE_BASIC_FETCH_FUNCS(memory);
-DECLARE_FETCH_FUNC(memory, string);
-DECLARE_FETCH_FUNC(memory, string_size);
 
 DECLARE_BASIC_FETCH_FUNCS(symbol);
 DECLARE_FETCH_FUNC(symbol, string);
