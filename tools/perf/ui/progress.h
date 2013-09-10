09ccb38d99be017938a1341bb0116e74c4e815ae
Namhyung Kim <namhyung.kim@lge.com>
2013-09-10 15:49:48 +0900

    perf ui/progress: Add new helper functions for progress bar
    Introduce ui_progress_setup() and ui_progress__advance() to separate
    out from the session logic.  It'll be used by other places in the
    upcoming patch.
    
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/ui/progress.h
+++ tools/perf/ui/progress.h
@@ -8,9 +8,19 @@ struct ui_progress {
 	void (*finish)(void);
 };
 
+struct perf_progress {
+	u64 curr;
+	u64 next;
+	u64 unit;
+	u64 total;
+};
+
 extern struct ui_progress *progress_fns;
 
 void ui_progress__init(void);
+
+void ui_progress__setup(struct perf_progress *p, u64 total);
+void ui_progress__advance(struct perf_progress *p, u64 adv, const char *title);
 
 void ui_progress__update(u64 curr, u64 total, const char *title);
 void ui_progress__finish(void);
