6a9b7b8f972bf1c0d6e0003d195ce799524835f7
Namhyung Kim <namhyung.kim@lge.com>
2013-12-19 13:35:57 +0900

    perf ui/tui: Implement header window
    Implement a simple, full-screen header window which shows session
    header (metadata) information.  Press 'i' key to display the header
    window.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/ui/browsers/hists.c
+++ tools/perf/ui/browsers/hists.c
@@ -1487,6 +1487,11 @@ static int perf_evsel__hists_browse(struct perf_evsel *evsel, int nr_events,
 		case 'l':
 			tui__log_window();
 			continue;
+		case 'i':
+			/* env->arch is NULL for live-mode (i.e. perf top) */
+			if (env->arch)
+				tui__header_window(env);
+			continue;
 		case K_F1:
 		case 'h':
 		case '?':
@@ -1510,6 +1515,7 @@ static int perf_evsel__hists_browse(struct perf_evsel *evsel, int nr_events,
 					"P             Print histograms to perf.hist.N\n"
 					"V             Verbose (DSO names in callchains, etc)\n"
 					"l             Show log messages\n"
+					"i             Show header information\n"
 					"/             Filter symbol by name");
 			continue;
 		case K_ENTER:
