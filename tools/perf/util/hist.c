ed5de1e5cf2f39388160dce30de108e1cc4e889e
Namhyung Kim <namhyung@kernel.org>
2013-12-26 17:44:10 +0900

    perf tools: Save callchain info for each cumulative entry
    When accumulating callchain entry, also save current snapshot of the
    chain so that it can show the rest of the chain.
    
    Tested-by: Arun Sharma <asharma@fb.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -745,6 +745,14 @@ iter_add_single_cumulative_entry(struct hist_entry_iter *iter,
 
 	he_cache[iter->curr++] = he;
 
+	callchain_append(he->callchain, &callchain_cursor, sample->period);
+
+	/*
+	 * We need to re-initialize the cursor since callchain_append()
+	 * advanced the cursor to the end.
+	 */
+	callchain_cursor_commit(&callchain_cursor);
+
 	return hist_entry__inc_addr_samples(he, evsel->idx, al->addr);
 }
 
@@ -757,8 +765,6 @@ iter_next_cumulative_entry(struct hist_entry_iter *iter,
 	node = callchain_cursor_current(&callchain_cursor);
 	if (node == NULL)
 		return 0;
-
-	callchain_cursor_advance(&callchain_cursor);
 
 	return fill_callchain_info(al, node, iter->hide_unresolved);
 }
@@ -783,6 +789,11 @@ iter_add_next_cumulative_entry(struct hist_entry_iter *iter,
 		.parent = iter->parent,
 	};
 	int i;
+	struct callchain_cursor cursor;
+
+	callchain_cursor_snapshot(&cursor, &callchain_cursor);
+
+	callchain_cursor_advance(&callchain_cursor);
 
 	/*
 	 * Check if there's duplicate entries in the callchain.
@@ -800,6 +811,8 @@ iter_add_next_cumulative_entry(struct hist_entry_iter *iter,
 		return -ENOMEM;
 
 	he_cache[iter->curr++] = he;
+
+	callchain_append(he->callchain, &cursor, sample->period);
 
 	return hist_entry__inc_addr_samples(he, evsel->idx, al->addr);
 }
