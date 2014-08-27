a9b2c4125dc619643b17baa541aeb44449731651
Namhyung Kim <namhyung@kernel.org>
2014-08-27 14:12:31 +0900

    perf tools: Maintain map groups list in a leader thread
    To support multi-threaded perf report, we need to maintain time-sorted
    map groups.  Add ->mg_list member to struct thread and sort the list
    by time.  Now leader threads have one more refcnt for map groups in
    the list so also update the thread-mg-share test case.
    
    Currently only add a new map groups when an exec (comm) event is
    received.
    
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/map.c
+++ tools/perf/util/map.c
@@ -471,6 +471,8 @@ void map_groups__init(struct map_groups *mg, struct machine *machine)
 	}
 	mg->machine = machine;
 	atomic_set(&mg->refcnt, 1);
+	mg->timestamp = 0;
+	INIT_LIST_HEAD(&mg->list);
 }
 
 static void __maps__purge(struct maps *maps)
@@ -527,6 +529,7 @@ struct map_groups *map_groups__new(struct machine *machine)
 void map_groups__delete(struct map_groups *mg)
 {
 	map_groups__exit(mg);
+	list_del(&mg->list);
 	free(mg);
 }
 
