a9f835a171bfbd7852340adae0094ed1a5cb1180
Namhyung Kim <namhyung@kernel.org>
2014-11-24 11:29:44 +0900

    perf record: Add --index option for building index table
    The new --index option will create indexed data file which can be
    processed by multiple threads parallelly.  It saves meta event and
    sample data in separate files and merges them with an index table.
    
    If there's an index table in the data file, the HEADER_DATA_INDEX
    feature bit is set and session->header.index[0] will point to the meta
    event area, and rest are sample data.  It'd look like below:
    
            +---------------------+
            |     file header     |
            |---------------------|
            |                     |
            |    meta events[0] <-+--+
            |                     |  |
            |---------------------|  |
            |                     |  |
            |    sample data[1] <-+--+
            |                     |  |
            |---------------------|  |
            |                     |  |
            |    sample data[2] <-|--+
            |                     |  |
            |---------------------|  |
            |         ...         | ...
            |---------------------|  |
            |     feature data    |  |
            |   (contains index) -+--+
            +---------------------+
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/header.c
+++ tools/perf/util/header.c
@@ -2652,6 +2652,8 @@ int perf_session__read_header(struct perf_session *session)
 						   session->tevent.pevent))
 		goto out_delete_evlist;
 
+	perf_has_index = perf_header__has_feat(&session->header, HEADER_DATA_INDEX);
+
 	return 0;
 out_errno:
 	return -errno;
