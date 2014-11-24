6e49d49c93e05ebaefb796ff9d49a003c8fefcd4
Namhyung Kim <namhyung@kernel.org>
2014-11-24 11:29:44 +0900

    perf record: Add --index option for building index table
    The new --index option will create indexed data file which can be
    processed by multiple threads parallelly.  It saves meta event and
    sample data in separate files and merges them with an index table.
    
    If there's an index table in the data file, the HEADER_DATA_INDEX
    feature bit is set and session->header.index[0] will point to the meta
    event area, and rest are sample data.  It'd look like below:
    
            +---------------------+
            |     file header     |
            |---------------------|
            |                     |
            |    meta events[0] <-+--+
            |                     |  |
            |---------------------|  |
            |                     |  |
            |    sample data[1] <-+--+
            |                     |  |
            |---------------------|  |
            |                     |  |
            |    sample data[2] <-|--+
            |                     |  |
            |---------------------|  |
            |         ...         | ...
            |---------------------|  |
            |     feature data    |  |
            |   (contains index) -+--+
            +---------------------+
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/perf.h
+++ tools/perf/perf.h
@@ -60,6 +60,7 @@ struct record_opts {
 	bool	     full_auxtrace;
 	bool	     auxtrace_snapshot_mode;
 	bool	     record_switch_events;
+	bool	     index;
 	unsigned int freq;
 	unsigned int mmap_pages;
 	unsigned int auxtrace_mmap_pages;
