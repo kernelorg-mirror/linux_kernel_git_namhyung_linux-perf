867c8c26d0a478e83cbcd6820e0b76f6fdff364d
Namhyung Kim <namhyung@kernel.org>
2014-12-01 14:43:16 +0900

    perf tools: Introduce machine__find*_thread_by_time()
    With data file indexing is enabled, it needs to search thread based on
    sample time since sample processing is done after other (task, comm and
    mmap) events are processed.  This can be a problem if a session is very
    long and pid is recycled - in that case it'll only see the last one.
    
    So keep thread start time in it, and search thread based on the time.
    This patch introduces machine__find{,new}_thread_by_time() function
    for this.  It'll first search current (i.e. recent) thread rbtree and
    then dead thread tree (and tid list).  If it couldn't find anyone,
    it'll create a new (missing) thread.
    
    The sample timestamp of 0 means that this is called from synthesized
    event so just use current rbtree.  The timestamp will be -1 if sample
    didn't record the timestamp so will see current threads automatically.
    
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/machine.h
+++ tools/perf/util/machine.h
@@ -75,8 +75,6 @@ static inline bool machine__kernel_ip(struct machine *machine, u64 ip)
 	return ip >= kernel_start;
 }
 
-struct thread *machine__find_thread(struct machine *machine, pid_t pid,
-				    pid_t tid);
 struct comm *machine__thread_exec_comm(struct machine *machine,
 				       struct thread *thread);
 
@@ -164,6 +162,14 @@ static inline bool machine__is_host(struct machine *machine)
 
 struct thread *__machine__findnew_thread(struct machine *machine, pid_t pid, pid_t tid);
 struct thread *machine__findnew_thread(struct machine *machine, pid_t pid, pid_t tid);
+struct thread *machine__find_thread(struct machine *machine, pid_t pid,
+				    pid_t tid);
+struct thread *machine__findnew_thread_by_time(struct machine *machine,
+					       pid_t pid, pid_t tid,
+					       u64 timestamp);
+struct thread *machine__find_thread_by_time(struct machine *machine,
+					    pid_t pid, pid_t tid,
+					    u64 timestamp);
 
 struct dso *machine__findnew_dso(struct machine *machine, const char *filename);
 
