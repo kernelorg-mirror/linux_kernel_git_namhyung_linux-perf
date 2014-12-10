75da3c3e8fc7de894f082a81b725989531615321
Namhyung Kim <namhyung@kernel.org>
2014-12-10 16:17:10 +0900

    perf tools: Fix progress ui to support multi thread
    Split ui_progress struct into global and local one.  Each thread
    updates local struct without lock and only updates global one if
    meaningful progress is done (with lock).
    
    To do that, pass struct ui_progress to __perf_session__process_event()
    and set it for the total size of multi-file storage.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -1481,7 +1481,8 @@ fetch_mmaped_event(struct perf_session *session,
 static int __perf_session__process_events(struct perf_session *session,
 					  struct events_stats *stats,
 					  u64 data_offset, u64 data_size,
-					  u64 file_size)
+					  u64 file_size,
+					  struct ui_progress *prog)
 {
 	struct ordered_events *oe = &session->ordered_events;
 	struct perf_tool *tool = session->tool;
@@ -1491,7 +1492,6 @@ static int __perf_session__process_events(struct perf_session *session,
 	size_t	mmap_size;
 	char *buf, *mmaps[NUM_MMAPS];
 	union perf_event *event;
-	struct ui_progress prog;
 	s64 skip;
 
 	perf_tool__fill_defaults(tool);
@@ -1502,8 +1502,6 @@ static int __perf_session__process_events(struct perf_session *session,
 
 	if (data_size && (data_offset + data_size < file_size))
 		file_size = data_offset + data_size;
-
-	ui_progress__init(&prog, file_size, "Processing events...");
 
 	mmap_size = MMAP_SIZE;
 	if (mmap_size > file_size) {
@@ -1570,7 +1568,7 @@ more:
 	head += size;
 	file_pos += size;
 
-	ui_progress__update(&prog, size);
+	ui_progress__update(prog, size);
 
 	if (session_done())
 		goto out;
@@ -1585,7 +1583,6 @@ out:
 		goto out_err;
 	err = auxtrace__flush_events(session, tool);
 out_err:
-	ui_progress__finish();
 	ordered_events__free(&session->ordered_events);
 	auxtrace__free_events(session);
 	session->one_mmap = false;
@@ -1594,11 +1591,14 @@ out_err:
 
 static int __perf_session__process_indexed_events(struct perf_session *session)
 {
+	struct ui_progress prog;
 	struct perf_data_file *file = session->file;
 	struct perf_tool *tool = session->tool;
 	u64 size = perf_data_file__size(file);
 	struct events_stats *stats = &session->evlist->stats;
 	int err = 0, i;
+
+	ui_progress__init(&prog, size, "Processing events...");
 
 	for (i = 0; i < (int)session->header.nr_index; i++) {
 		struct perf_file_section *idx = &session->header.index[i];
@@ -1616,17 +1616,20 @@ static int __perf_session__process_indexed_events(struct perf_session *session)
 
 		err = __perf_session__process_events(session, stats,
 						     idx->offset,
-						     idx->size, size);
+						     idx->size, size, &prog);
 		if (err < 0)
 			break;
 	}
 
+	ui_progress__finish();
 	perf_session__warn_about_errors(session, stats);
+
 	return err;
 }
 
 int perf_session__process_events(struct perf_session *session)
 {
+	struct ui_progress prog;
 	struct perf_data_file *file = session->file;
 	u64 size = perf_data_file__size(file);
 	struct events_stats *stats = &session->evlist->stats;
@@ -1640,14 +1643,41 @@ int perf_session__process_events(struct perf_session *session)
 	if (perf_has_index)
 		return __perf_session__process_indexed_events(session);
 
+	ui_progress__init(&prog, size, "Processing events...");
+
 	err = __perf_session__process_events(session, stats,
 					     session->header.data_offset,
 					     session->header.data_size,
-					     size);
+					     size, &prog);
 
+	ui_progress__finish();
 	perf_session__warn_about_errors(session, stats);
+
 	return err;
 }
+
+struct ui_progress_ops *orig_progress__ops;
+
+static void mt_progress__update(struct ui_progress *p)
+{
+	struct perf_tool_mt *mt_tool = container_of(p, struct perf_tool_mt, prog);
+	struct ui_progress *gprog = mt_tool->global_prog;
+	static pthread_mutex_t prog_lock = PTHREAD_MUTEX_INITIALIZER;
+
+	pthread_mutex_lock(&prog_lock);
+
+	gprog->curr += p->step;
+	if (gprog->curr >= gprog->next) {
+		gprog->next += gprog->step;
+		orig_progress__ops->update(gprog);
+	}
+
+	pthread_mutex_unlock(&prog_lock);
+}
+
+static struct ui_progress_ops mt_progress__ops = {
+	.update = mt_progress__update,
+};
 
 static void *processing_thread_idx(void *arg)
 {
@@ -1657,9 +1687,12 @@ static void *processing_thread_idx(void *arg)
 	u64 size = session->header.index[mt_tool->idx].size;
 	u64 file_size = perf_data_file__size(session->file);
 
+	ui_progress__init(&mt_tool->prog, size, "");
+
 	pr_debug("processing samples using thread [%d]\n", mt_tool->idx);
 	if (__perf_session__process_events(session, &mt_tool->stats,
-					   offset, size, file_size) < 0) {
+					   offset, size, file_size,
+					   &mt_tool->prog) < 0) {
 		pr_err("processing samples failed (thread [%d])\n", mt_tool->idx);
 		return NULL;
 	}
@@ -1679,7 +1712,8 @@ int perf_session__process_events_mt(struct perf_session *session, void *arg)
 	struct perf_tool_mt *mt_tools = NULL;
 	struct perf_session *ms;
 	struct perf_tool_mt *mt;
-	pthread_t *th_id;
+	struct ui_progress prog;
+	pthread_t *th_id = NULL;
 	int err, i, k;
 	int nr_index = session->header.nr_index;
 	u64 size = perf_data_file__size(file);
@@ -1692,12 +1726,18 @@ int perf_session__process_events_mt(struct perf_session *session, void *arg)
 	if (perf_session__register_idle_thread(session) == NULL)
 		return -ENOMEM;
 
+	ui_progress__init(&prog, size, "Processing events...");
+
 	err = __perf_session__process_events(session, &evlist->stats,
 					     session->header.index[0].offset,
 					     session->header.index[0].size,
-					     size);
+					     size, &prog);
 	if (err)
-		return err;
+		goto out;
+
+	orig_progress__ops = ui_progress__ops;
+	ui_progress__ops = &mt_progress__ops;
+	ui_progress__ops->finish = orig_progress__ops->finish;
 
 	th_id = calloc(nr_index, sizeof(*th_id));
 	if (th_id == NULL)
@@ -1734,6 +1774,7 @@ int perf_session__process_events_mt(struct perf_session *session, void *arg)
 		mt->session = ms;
 		mt->idx = i;
 		mt->priv = arg;
+		mt->global_prog = &prog;
 
 		pthread_create(&th_id[i], NULL, processing_thread_idx, mt);
 	}
@@ -1757,6 +1798,9 @@ int perf_session__process_events_mt(struct perf_session *session, void *arg)
 		}
 	}
 
+	ui_progress__ops = orig_progress__ops;
+	ui_progress__init(&prog, nr_entries, "Merging related events...");
+
 	for (i = 1; i < nr_index; i++) {
 		mt = &mt_tools[i];
 
@@ -1766,7 +1810,7 @@ int perf_session__process_events_mt(struct perf_session *session, void *arg)
 			if (perf_evsel__is_dummy_tracking(evsel))
 				continue;
 
-			hists__mt_resort(hists, &mt->hists[evsel->idx]);
+			hists__mt_resort(hists, &mt->hists[evsel->idx], &prog);
 
 			/* Non-group events are considered as leader */
 			if (symbol_conf.event_group &&
@@ -1781,6 +1825,7 @@ int perf_session__process_events_mt(struct perf_session *session, void *arg)
 	}
 
 out:
+	ui_progress__finish();
 	perf_session__warn_about_errors(session, &evlist->stats);
 
 	if (mt_tools) {
