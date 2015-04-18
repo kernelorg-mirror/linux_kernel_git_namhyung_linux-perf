5e8d91700515ef9f2c7d218ec292e1017522f260
Namhyung Kim <namhyung@kernel.org>
2015-04-19 00:35:03 +0900

    perf hists browser: Simplify zooming code a bit
    Introduce pstack_peek() and reuse do_zoom_dso/thread() function.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/ui/browsers/hists.c
+++ tools/perf/ui/browsers/hists.c
@@ -1877,17 +1877,11 @@ static int perf_evsel__hists_browse(struct perf_evsel *evsel, int nr_events,
 					goto out_free_stack;
 				continue;
 			}
-			top = pstack__pop(fstack);
-			if (top == &browser->hists->dso_filter) {
-				perf_hpp__set_elide(HISTC_DSO, false);
-				browser->hists->dso_filter = NULL;
-				ui_helpline__pop();
-			}
-			if (top == &browser->hists->thread_filter) {
-				perf_hpp__set_elide(HISTC_THREAD, false);
-				thread__zput(browser->hists->thread_filter);
-				ui_helpline__pop();
-			}
+			top = pstack__peek(fstack);
+			if (top == &browser->hists->dso_filter)
+				do_zoom_dso(opts, browser, hbt, fstack, env);
+			if (top == &browser->hists->thread_filter)
+				do_zoom_thread(opts, browser, hbt, fstack, env);
 			continue;
 		}
 		case K_ESC:
