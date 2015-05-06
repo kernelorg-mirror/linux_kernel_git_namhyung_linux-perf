07b3b220d25609eb7d7d27b55199cd24e87d514e
Namhyung Kim <namhyung@kernel.org>
2015-05-06 10:45:04 +0900

    perf tools: Add testcase for managing maps with time
    This tests new map_groups__{insert,find}_by_time() API working
    correctly by using 3 * 100 maps.
    
    Cc: Stephane Eranian <eranian@google.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -68,6 +68,7 @@ int test_session_topology(void);
 int test__thread_comm(void);
 int test__thread_lookup_time(void);
 int test__thread_mg_time(void);
+int test__thread_map_lookup_time(void);
 
 #if defined(__x86_64__) || defined(__i386__) || defined(__arm__) || defined(__aarch64__)
 #ifdef HAVE_DWARF_UNWIND_SUPPORT
