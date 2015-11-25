bc904e228f4398ac3076503f48694bb0db2d0fce
Namhyung Kim <namhyung@kernel.org>
2015-11-26 06:23:45 +0900

    perf callchain: Honor hide_unresolved
    If user requested to hide unresolved entries, skip unresolved callchains
    as well as hist entries.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/machine.c
+++ tools/perf/util/machine.c
@@ -1618,6 +1618,8 @@ static int add_callchain_ip(struct thread *thread,
 		}
 	}
 
+	if (symbol_conf.hide_unresolved && al.sym == NULL)
+		return 0;
 	return callchain_cursor_append(&callchain_cursor, al.addr, al.map, al.sym);
 }
 
@@ -1885,6 +1887,9 @@ check_calls:
 static int unwind_entry(struct unwind_entry *entry, void *arg)
 {
 	struct callchain_cursor *cursor = arg;
+
+	if (symbol_conf.hide_unresolved && entry->sym == NULL)
+		return 0;
 	return callchain_cursor_append(cursor, entry->ip,
 				       entry->map, entry->sym);
 }
