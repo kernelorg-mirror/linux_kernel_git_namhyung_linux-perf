87d13a037f6e74598011df84b510428d9b4286d3
Namhyung Kim <namhyung@kernel.org>
2017-06-15 20:56:03 +0900

    perf symbols: Load kernel module symbols ASAP
    When loading kernel symbols from /proc/kallsyms, it might have different
    addresses for modules.  We should honor the mmap event recorded in a
    perf.data so load the module symbols when it sees the event so that it
    cannot be overridden by symbols in /proc/kallsyms later.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Wang Nan <wangnan0@huawei.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/machine.c
+++ tools/perf/util/machine.c
@@ -656,6 +656,9 @@ struct map *machine__findnew_module_map(struct machine *machine, u64 start,
 
 	map_groups__insert(&machine->kmaps, map);
 
+	if (dso->has_build_id)
+		map__load(map);
+
 	/* Put the map here because map_groups__insert alread got it */
 	map__put(map);
 out:
