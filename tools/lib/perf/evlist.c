dde98404351c6d82ab9e0f04a9c69927f0899a6b
Namhyung Kim <namhyung@kernel.org>
2022-09-19 22:46:38 -0700

    libperf: Propagate maps only if necessary
    The current code propagate evsel's cpu map settings to evlist when it's
    added to an evlist.  But the evlist->all_cpus and each evsel's cpus will
    be updated in perf_evlist__set_maps() later.  No need to do it before
    evlist's cpus are set actually.
    
    Actually we discarded this intermediate all_cpus maps at the beginning
    of perf_evlist__set_maps().  Let's not do this.  It's only needed when
    an evsel is added after the evlist cpu maps are set.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/lib/perf/evlist.c
+++ tools/lib/perf/evlist.c
@@ -67,10 +67,6 @@ static void perf_evlist__propagate_maps(struct perf_evlist *evlist)
 {
 	struct perf_evsel *evsel;
 
-	/* Recomputing all_cpus, so start with a blank slate. */
-	perf_cpu_map__put(evlist->all_cpus);
-	evlist->all_cpus = NULL;
-
 	perf_evlist__for_each_evsel(evlist, evsel)
 		__perf_evlist__propagate_maps(evlist, evsel);
 }
@@ -81,7 +77,9 @@ void perf_evlist__add(struct perf_evlist *evlist,
 	evsel->idx = evlist->nr_entries;
 	list_add_tail(&evsel->node, &evlist->entries);
 	evlist->nr_entries += 1;
-	__perf_evlist__propagate_maps(evlist, evsel);
+
+	if (evlist->all_cpus)
+		__perf_evlist__propagate_maps(evlist, evsel);
 }
 
 void perf_evlist__remove(struct perf_evlist *evlist,
