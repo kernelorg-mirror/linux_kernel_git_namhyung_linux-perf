3c6bea04f543a752a830a76afbccf541d3786b37
Namhyung Kim <namhyung@kernel.org>
2022-10-07 19:22:15 -0700

    perf tools: Save evsel->pmu in parse_events()
    Now evsel has a pmu pointer, let's save the info and use it like in
    evsel__find_pmu().
    
    Acked-by: Ian Rogers <irogers@google.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -467,6 +467,7 @@ struct evsel *evsel__clone(struct evsel *orig)
 	evsel->collect_stat = orig->collect_stat;
 	evsel->weak_group = orig->weak_group;
 	evsel->use_config_name = orig->use_config_name;
+	evsel->pmu = orig->pmu;
 
 	if (evsel__copy_config_terms(evsel, orig) < 0)
 		goto out_err;
