2e97e6b5b178f651ecc08262d836ae1a4aa7ffc0
Namhyung Kim <namhyung@kernel.org>
2023-02-10 15:38:27 -0800

    perf bpf filter: Add 'pid' sample data support
    The pid is special because it's saved in the PERF_SAMPLE_TID together.
    So it needs to differenciate tid and pid using the 'part' field in the
    perf bpf filter entry struct.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/bpf-filter.c
+++ tools/perf/util/bpf-filter.c
@@ -43,6 +43,7 @@ int perf_bpf_filter__prepare(struct evsel *evsel)
 	list_for_each_entry(expr, &evsel->bpf_filters, list) {
 		struct perf_bpf_filter_entry entry = {
 			.op = expr->op,
+			.part = expr->part,
 			.flags = expr->sample_flags,
 			.value = expr->val,
 		};
@@ -83,7 +84,7 @@ u64 perf_bpf_filter__lost_count(struct evsel *evsel)
 	return skel ? skel->bss->dropped : 0;
 }
 
-struct perf_bpf_filter_expr *perf_bpf_filter_expr__new(unsigned long sample_flags,
+struct perf_bpf_filter_expr *perf_bpf_filter_expr__new(unsigned long sample_flags, int part,
 						       enum perf_bpf_filter_op op,
 						       unsigned long val)
 {
@@ -92,6 +93,7 @@ struct perf_bpf_filter_expr *perf_bpf_filter_expr__new(unsigned long sample_flag
 	expr = malloc(sizeof(*expr));
 	if (expr != NULL) {
 		expr->sample_flags = sample_flags;
+		expr->part = part;
 		expr->op = op;
 		expr->val = val;
 	}
