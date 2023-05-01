91bd75667f0d6792a40d5dd8008100a26352df69
Namhyung Kim <namhyung@kernel.org>
2023-05-01 15:27:51 -0700

    perf dwarf-aux: Factor out __die_get_typename()
    The __die_get_typename() is to get the name of the given DIE in C-style
    type name.  The difference from the die_get_typename() is that it does
    not retrieve the DW_AT_type and use the given DIE directly.  This will
    be used when users know the type DIE already.
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/dwarf-aux.h
+++ tools/perf/util/dwarf-aux.h
@@ -116,6 +116,9 @@ Dwarf_Die *die_find_variable_at(Dwarf_Die *sp_die, const char *name,
 Dwarf_Die *die_find_member(Dwarf_Die *st_die, const char *name,
 			   Dwarf_Die *die_mem);
 
+/* Get the name of given type DIE */
+int __die_get_typename(Dwarf_Die *type, struct strbuf *buf);
+
 /* Get the name of given variable DIE */
 int die_get_typename(Dwarf_Die *vr_die, struct strbuf *buf);
 
