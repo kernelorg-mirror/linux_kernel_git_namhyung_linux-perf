f8cbd8f5243ed0e4dfc2a5d26b5a16aad0877810
Namhyung Kim <namhyung@kernel.org>
2023-05-02 16:44:43 -0700

    perf annotate: Add --type-stat option for debugging
    The --type-stat option is to be used with --data-type and to print
    detailed failure reasons for the data type annotation.
    
      $ perf annotate --data-type --type-stat
      Annotate data type stats:
      total 294, ok 116 (39.5%), bad 178 (60.5%)
      -----------------------------------------------------------
              30 : no_sym
              40 : no_insn_ops
              33 : no_mem_ops
              63 : no_var
               4 : no_typeinfo
               8 : bad_offset
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/annotate.c
+++ tools/perf/util/annotate.c
@@ -3649,11 +3649,17 @@ struct annotated_data_type *hist_entry__get_data_type(struct hist_entry *he)
 	u64 ip = he->ip;
 	int i;
 
-	if (ms->map == NULL || ms->sym == NULL)
-		return NULL;
+	ann_data_stat.total++;
 
-	if (evsel__get_arch(evsel, &arch) < 0)
+	if (ms->map == NULL || ms->sym == NULL) {
+		ann_data_stat.no_sym++;
 		return NULL;
+	}
+
+	if (evsel__get_arch(evsel, &arch) < 0) {
+		ann_data_stat.no_insn++;
+		return NULL;
+	}
 
 	/* Make sure it runs objdump to get disasm of the function */
 	symbol__ensure_annotate(ms, evsel);
@@ -3663,11 +3669,15 @@ struct annotated_data_type *hist_entry__get_data_type(struct hist_entry *he)
 	 * This is too slow...
 	 */
 	dl = find_disasm_line(ms->sym, ip);
-	if (dl == NULL)
+	if (dl == NULL) {
+		ann_data_stat.no_insn++;
 		return NULL;
+	}
 
-	if (annotate_get_insn_location(arch, dl, &loc) < 0)
+	if (annotate_get_insn_location(arch, dl, &loc) < 0) {
+		ann_data_stat.no_insn_ops++;
 		return NULL;
+	}
 
 	for_each_insn_op_loc(&loc, i, op_loc) {
 		if (!op_loc->mem_ref)
@@ -3684,5 +3694,7 @@ struct annotated_data_type *hist_entry__get_data_type(struct hist_entry *he)
 		he->mem_type_off = op_loc->offset;
 		return mem_type;
 	}
+
+	ann_data_stat.no_mem_ops++;
 	return NULL;
 }
