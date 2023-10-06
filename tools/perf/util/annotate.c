8af5a95f06015ba763049bb142abf37cddc7010c
Namhyung Kim <namhyung@kernel.org>
2023-10-06 14:42:55 -0700

    perf annotate-data: Add stack canary type
    When the stack protector is enabled, compiler would generate code to
    check stack overflow with a special value called 'stack carary' at
    runtime.  On x86_64, GCC hard-codes the stack canary as %gs:40.
    
    While there's a definition of fixed_percpu_data in asm/processor.h,
    it seems that the header is not included everywhere and many places
    it cannot find the type info.  As it's in the well-known location (at
    %gs:40), let's add a pseudo stack canary type to handle it specially.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/annotate.c
+++ tools/perf/util/annotate.c
@@ -113,6 +113,13 @@ struct annotated_data_type stackop_type = {
 	},
 };
 
+struct annotated_data_type canary_type = {
+	.self = {
+		.type_name = (char *)"(stack canary)",
+		.children = LIST_HEAD_INIT(canary_type.self.children),
+	},
+};
+
 static int arch__grow_instructions(struct arch *arch)
 {
 	struct ins *new_instructions;
@@ -3768,6 +3775,17 @@ static bool is_stack_operation(struct arch *arch, struct disasm_line *dl)
 	return false;
 }
 
+static bool is_stack_canary(struct arch *arch, struct annotated_op_loc *loc)
+{
+	/* On x86_64, %gs:40 is used for stack canary */
+	if (arch__is(arch, "x86")) {
+		if (loc->segment == INSN_SEG_X86_GS && loc->offset == 40)
+			return true;
+	}
+
+	return false;
+}
+
 u64 annotate_calc_pcrel(struct map_symbol *ms, u64 ip, int offset,
 			struct disasm_line *dl)
 {
@@ -3937,6 +3955,12 @@ retry:
 		}
 
 		mem_type = find_data_type(&dloc);
+
+		if (mem_type == NULL && is_stack_canary(arch, op_loc)) {
+			mem_type = &canary_type;
+			dloc.type_offset = 0;
+		}
+
 		if (mem_type)
 			istat->good++;
 		else
