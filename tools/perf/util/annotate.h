83be83af98c622ab419a8741fb624e83b9fcae21
Namhyung Kim <namhyung@kernel.org>
2023-10-06 13:06:19 -0700

    perf annotate: Parse x86 segment register location
    Add a segment field in the struct annotated_insn_loc and save it for the
    segment based addressing like %gs:0x28.  For simplicity it now handles
    %gs register only.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/annotate.h
+++ tools/perf/util/annotate.h
@@ -446,6 +446,7 @@ int annotate_check_args(void);
  * @reg1: First register in the operand
  * @reg2: Second register in the operand
  * @offset: Memory access offset in the operand
+ * @segment: Segment selector register
  * @mem_ref: Whether the operand accesses memory
  * @multi_regs: Whether the second register is used
  */
@@ -453,6 +454,7 @@ struct annotated_op_loc {
 	int reg1;
 	int reg2;
 	int offset;
+	u8 segment;
 	bool mem_ref;
 	bool multi_regs;
 };
@@ -462,6 +464,17 @@ enum annotated_insn_ops {
 	INSN_OP_TARGET = 1,
 
 	INSN_OP_MAX,
+};
+
+enum annotated_x86_segment {
+	INSN_SEG_NONE = 0,
+
+	INSN_SEG_X86_CS,
+	INSN_SEG_X86_DS,
+	INSN_SEG_X86_ES,
+	INSN_SEG_X86_FS,
+	INSN_SEG_X86_GS,
+	INSN_SEG_X86_SS,
 };
 
 /**
