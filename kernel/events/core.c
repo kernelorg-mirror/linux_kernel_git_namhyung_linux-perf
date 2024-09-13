8d4cb42a20eb949b5ee5730cee588237e9c8df48
Josh Poimboeuf <jpoimboe@kernel.org>
2024-09-14 01:02:09 +0200

    perf: Remove get_perf_callchain() 'crosstask' argument
    get_perf_callchain() doesn't support cross-task unwinding, so it doesn't
    make much sense to have 'crosstask' as an argument.  Instead, have
    perf_callchain() adjust 'user' accordingly.
    
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7680,16 +7680,16 @@ perf_callchain(struct perf_event *event, struct pt_regs *regs)
 {
 	bool kernel = !event->attr.exclude_callchain_kernel;
 	bool user   = !event->attr.exclude_callchain_user;
-	/* Disallow cross-task user callchains. */
-	bool crosstask = event->ctx->task && event->ctx->task != current;
 	const u32 max_stack = event->attr.sample_max_stack;
 	struct perf_callchain_entry *callchain;
+
+	/* Disallow cross-task user callchains. */
+	user &= !event->ctx->task || event->ctx->task == current;
 
 	if (!kernel && !user)
 		return &__empty_callchain;
 
-	callchain = get_perf_callchain(regs, kernel, user,
-				       max_stack, crosstask, true);
+	callchain = get_perf_callchain(regs, kernel, user, max_stack, true);
 	return callchain ?: &__empty_callchain;
 }
 
